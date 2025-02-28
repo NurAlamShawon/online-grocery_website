c98fbd4669a54ece6b2f3d3090ec3da319c7ffd2
Nur Alam Shawon <ahsan.shawon9@gmail.com>
2025-02-28 12:20:14 +0600

    grid part

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
